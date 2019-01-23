ad7db1e0f4578bd4e4743c7233b4922cc080747b
sheylao <sheypr85@gmail.com>
2019-01-22 22:00:02 -0600

    Making changes

---
+++ server.js
@@ -0,0 +1,20 @@
+// Dependencies
+var express = require("express");
+
+
+var app = express();
+var PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+require("./app/routing/htmlRoutes")(app);
+
+
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
