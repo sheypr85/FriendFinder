d6124b8e6993d659a904f84b27595c63321359ac
sheylao <sheypr85@gmail.com>
2019-01-23 02:07:09 -0600

    Adding changes

--- app/data/friends.js
+++ app/data/friends.js
@@ -14,7 +14,55 @@ var friendsData = [
         4,
         3
       ]
-    }
+    },
+    {
+        name: "Sandra",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores: [
+          4,
+          5,
+          5,
+          5,
+          5,
+          5,
+          5,
+          5,
+          5,
+          1
+        ]
+      },
+      {
+        name: "Kirk",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores: [
+          5,
+          5,
+          4,
+          5,
+          4,
+          3,
+          5,
+          5,
+          5,
+          5
+        ]
+      },
+      {
+        name: "Maria",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores: [
+          2,
+          3,
+          3,
+          4,
+          2,
+          3,
+          3,
+          3,
+          3,
+          2
+        ]
+      },
   ];
   
   // Note how we export the array. This makes it accessible to other files using require.
