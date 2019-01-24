af4d149f82ddc05a691077d0fad8d3de14e6efa4
sheylao <sheypr85@gmail.com>
2019-01-23 19:30:20 -0600

    Adding more code

--- app/data/friends.js
+++ app/data/friends.js
@@ -63,6 +63,22 @@ var friendsData = [
           2
         ]
       },
+      {
+        name: "Shey",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores: [
+          1,
+          1,
+          3,
+          4,
+          2,
+          3,
+          5,
+          3,
+          3,
+          2
+        ]
+      },
   ];
   
   // Note how we export the array. This makes it accessible to other files using require.
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -9,7 +9,133 @@ module.exports = function(app) {
 
       app.post("/api/friends", function(req, res) {
         friendsData.push(req.body);
+
+
         res.json(friendsData)
       });
 }
 
+
+function compareScores(a, b) {
+    var result = 0;
+    for(var i = 0; i < a.length; i++) {
+       result = result + Math.abs(a[i]- b[i]);
+    }
+    return result;
+}
+
+
+function matchFriends(newFriend){
+    var scoreMatch = [];
+    for (var i = 0; i < friendsData.length; i++) {
+        var resultHere = compareScores(friendsData[i].scores, newFriend.scores)
+        var friendInfo = {
+            name: friendsData[i].name,
+            score: resultHere
+        }
+        console.log(friendInfo)
+    }
+}
+
+
+
+var newFriend = {
+        name: "Maria",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores: [
+          2,
+          3,
+          3,
+          4,
+          2,
+          3,
+          3,
+          3,
+          3,
+          2
+        ]
+}
+
+
+var friendsData = [
+    {
+      name: "John",
+      photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+      scores: [
+        5,
+        4,
+        3,
+        4,
+        5,
+        3,
+        1,
+        5,
+        4,
+        3
+      ]
+    },
+    {
+        name: "Sandra",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores: [
+          4,
+          5,
+          5,
+          5,
+          5,
+          5,
+          5,
+          5,
+          5,
+          1
+        ]
+      },
+      {
+        name: "Kirk",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores: [
+          5,
+          5,
+          4,
+          5,
+          4,
+          3,
+          5,
+          5,
+          5,
+          5
+        ]
+      },
+      {
+        name: "Maria",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores: [
+          2,
+          3,
+          3,
+          4,
+          2,
+          3,
+          3,
+          3,
+          3,
+          2
+        ]
+      },
+      {
+        name: "Shey",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores: [
+          1,
+          1,
+          3,
+          4,
+          2,
+          3,
+          5,
+          3,
+          3,
+          2
+        ]
+      },
+  ];
