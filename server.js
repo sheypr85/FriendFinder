bef22621d6bbb702e1f33725a5a9923fa23de8d1
sheylao <sheypr85@gmail.com>
2019-01-26 13:36:46 -0600

    Adding changes

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
+//ROUTER
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
 
